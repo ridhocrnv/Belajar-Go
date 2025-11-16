7abf53943a3ccdb147413223e14000e12e0ae237
ridhocrnv <ridhoahmadirawan281@gmail.com>
2025-11-16 23:28:16 +0800

    Push: Hari ke-1 Belajar Golang Dasar. 02

---
+++ GOLANG DASAR/Latihan/Latihan1.go
@@ -0,0 +1,27 @@
+/* 
+
+🏕️ Project Latihan Modul 1: Si Penyapa
+Kamu sudah berhasil membuat komputer menyapa "Dunia". Tapi bagaimana jika kamu ingin komputer menyapamu?
+
+Tugasmu:
+1. Ubah program main.go di atas.
+2. Ganti "Halo, Dunia!" menjadi "Halo, nama saya [Isi Namamu]!".
+3. Jalankan lagi dengan go run main.go.
+
+Tantangan Pengembangan (Cara Mengembangkan):
+	Bisakah kamu membuat program itu mencetak DUA baris?
+	Baris pertama: "Halo, Petualang!"
+	Baris kedua: "Saya siap belajar Go!"
+	(Petunjuk: Gunakan fmt.Println() lagi di baris baru).
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Halo, nama saya Alex")
+	fmt.Println("Halo, Petualang")
+	fmt.Println("Saya siap belajar Go!")
+}
